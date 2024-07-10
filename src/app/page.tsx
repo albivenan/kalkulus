8ab87432d9223eadc2893ec8c09b4f583dc1f6c5
albivenan <149981434+albivenan@users.noreply.github.com>
2024-07-10 07:58:38 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,16 +23,16 @@ const atan2 = (y: number, x: number): number => {
   return 0.0;
 };
 
-const degrees = (radians: number): number => radians * 180.0 / Math.PI;
-const radians = (degrees: number): number => degrees * Math.PI / 180.0;
+const derajat = (radians: number): number => radians * 180.0 / Math.PI;
+const radians = (derajat: number): number => derajat * Math.PI / 180.0;
 
-const CartesianToPolar = (x: number, y: number): [number, number] => {
+const kartesiusKePolar = (x: number, y: number): [number, number] => {
   const r = sqrt(x**2 + y**2);
-  const theta = degrees(atan2(y, x));
+  const theta = derajat(atan2(y, x));
   return [r, theta];
 };
 
-const PolarToCartesian = (r: number, theta: number): [number, number] => {
+const polarKeKartesius = (r: number, theta: number): [number, number] => {
   const thetaRad = radians(theta);
   const x = r * Math.cos(thetaRad);
   const y = r * Math.sin(thetaRad);
@@ -40,27 +40,27 @@ const PolarToCartesian = (r: number, theta: number): [number, number] => {
 };
 
 const Home = () => {
-  const [conversionType, setConversionType] = useState<string>('1');
+  const [jenisKonversi, setJenisKonversi] = useState<string>('1');
   const [x, setX] = useState<number>(0);
   const [y, setY] = useState<number>(0);
   const [r, setR] = useState<number>(0);
-  const [result, setResult] = useState<string | null>(null);
+  const [hasil, setHasil] = useState<string | null>(null);
   const [shaking, setIsShaking] = useState<boolean>(false);
   const [theta, setTheta] = useState<number>(0);
 
   
 
-  const handleConversion = () => {
+  const handleJenisPertanyaan = () => {
     setIsShaking(true);
     setTimeout(() => {
     setIsShaking(false);
   }, 500);
-    if (conversionType === '1') {
-      const [r, theta] = CartesianToPolar(x, y);
-      setResult(`Koordinat kartesius (${x}, ${y}) dalam koordinat polar adalah (r = ${r}, θ = ${theta} derajat)`);
-    } else if (conversionType === '2') {
-      const [x, y] = PolarToCartesian(r, theta);
-      setResult(`Koordinat polar (r = ${r}, θ = ${theta} derajat) dalam koordinat kartesius adalah (x = ${x}, y = ${y})`);
+    if (jenisKonversi === '1') {
+      const [r, theta] = kartesiusKePolar(x, y);
+      setHasil(`Koordinat kartesius dengan nilai (${x}, ${y}) pada koordinat polar adalah sebagai berikut: (r = ${r}, θ = ${theta} derajat)`);
+    } else if (jenisKonversi === '2') {
+      const [x, y] = polarKeKartesius(r, theta);
+      setHasil(`Koordinat polar dengan nilai (r = ${r}, θ = ${theta} derajat) pada koordinat kartesius adalah sebagai berikut: (x = ${x}, y = ${y})`);
     }
   };
 
@@ -75,7 +75,7 @@ const Home = () => {
           <p className='text-sm'>Dosen Pengampu: Buang Budi Wahono, S. Kom., M. Si.</p>
         </div>
         <div className="w-[30vh] h-[30vh] flex justify-center items-center image">
-          <Image src={profile} loading="lazy" placeholder="blur" className='hover:opacity-50 duration-200' style={{width: '20vh', height: '20vh', borderRadius: '100%', objectFit: 'cover', objectPosition: 'top', transform: 'rotate(-40deg)'}} alt='profile' />
+          <Image src={profile} className='hover:opacity-50 duration-200' style={{width: '20vh', height: '20vh', borderRadius: '100%', objectFit: 'cover', objectPosition: 'top', transform: 'rotate(-40deg)'}} alt='profile' />
         </div>
         <div className="-mt-4 text-sm text-center flex gap-x-8">
           <p>Gei Zhinjian Albivenanza</p>
@@ -98,12 +98,12 @@ const Home = () => {
       <h1 className='text-3xl font-extrabold text-center mb-2'>Aplikasi Konversi koordinat Polar ke Kartesius dan Sebaliknya</h1>
       <div className='grid gap-y-2'>
         <label className='font-bold text-lg'>Pilih jenis konversi:</label>
-        <select value={conversionType} onChange={(e) => setConversionType(e.target.value)} className='p-2 rounded-xl focus:border-neutral-700'>
+        <select value={jenisKonversi} onChange={(e) => setJenisKonversi(e.target.value)} className='p-2 rounded-xl focus:border-neutral-700'>
           <option value="1">Kartesius ke Polar</option>
           <option value="2">Polar ke Kartesius</option>
         </select>
       </div>
-      {conversionType === '1' && (
+      {jenisKonversi === '1' && (
         <div className='grid gap-y-4'>
           <div className='grid gap-y-2'>
             <label className='font-bold'>Masukkan koordinat x: </label>
@@ -115,7 +115,7 @@ const Home = () => {
           </div>
         </div>
       )}
-      {conversionType === '2' && (
+      {jenisKonversi === '2' && (
         <div className='grid gap-y-4'>
           <div className='grid gap-y-2'>
             <label className='font-bold text-lg'>Masukkan koordinat r: </label>
@@ -127,10 +127,10 @@ const Home = () => {
           </div>
         </div>
       )}
-      <button onClick={handleConversion} className='font-bold text-xl bg-neutral-700 text-white px-4 py-2 w-[60%] mx-auto rounded-full hover:bg-neutral-900 duration-300'>Convert</button>
+      <button onClick={handleJenisPertanyaan} className='font-bold text-xl bg-neutral-700 text-white px-4 py-2 w-[60%] mx-auto rounded-full hover:bg-neutral-900 duration-300'>Convert</button>
       <div className={`relative border-2 border-black px-2 py-6 rounded-2xl mt-4 bg-white ${shaking && 'shake'}`}>
         <p className="absolute font-bold text-lg -top-[30%] bg-neutral-700 text-white px-4 py-2 rounded-xl">Hasil:</p>
-        {result ? <p className='font-bold px-2'>{result}</p> : <p className='opacity-50 px-2'>Opsss .... Silakan masukkan angka terlebih dahulu untuk mengetahui hasil konversi</p>
+        {hasil ? <p className='font-bold px-2'>{hasil}</p> : <p className='opacity-50 px-2'>Opsss .... Silakan masukkan angka terlebih dahulu untuk mengetahui hasil konversi</p>
 }
       </div>
       </div>
